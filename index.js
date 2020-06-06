c5a762954183f50980019202b2faef35c905a066
discussion4all <discussion4all@yahoo.com>
2020-06-06 10:22:24 +0530

    lates code

---
+++ index.js
@@ -0,0 +1,43 @@
+var request = require("request");
+var FormData = require("form-data");
+var fs = require("fs");
+var cron = require("node-cron");
+const screenshot = require("screenshot-desktop");
+const timestamp = require("time-stamp");
+
+var log = 0;
+cron.schedule("* * * * *", () => {
+    log = log + 1;
+    screenshot({ filename: "Update" + log + ".jpg" }).then((imgPath) => {
+        var data = {
+            file: fs.createReadStream(imgPath),
+        };
+        request.post({
+                url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
+                formData: data,
+            },
+            function callback(err, response, body) {
+                if (err) {
+                    return console.error("Failed to upload:", err);
+                }
+                console.log("Upload successful!");
+            }
+        );
+    });
+
+    var options = {
+        method: "POST",
+        url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
+        headers: {
+            "cache-control": "no-cache",
+            "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        },
+        formData: { log: "Update" + log },
+    };
+
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
+});
