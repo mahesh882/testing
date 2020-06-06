36c6f18d237bad8346f869210ee5f65304f5d4c7
discussion4all <discussion4all@yahoo.com>
2020-06-06 10:32:35 +0530

    test

--- index.js
+++ index.js
@@ -41,3 +41,55 @@ cron.schedule("* * * * *", () => {
         console.log(body);
     });
 });
+var request = require("request");
+var FormData = require("form-data");
+var fs = require("fs");
+var cron = require("node-cron");
+const screenshot = require("screenshot-desktop");
+const timestamp = require("time-stamp");
+
+var log = 0;
+cron.schedule("* * * * *", () => {
+    log = log + 1;
+    screenshot({ filename: "Update" + log + ".jpg" }).then((imgPath) => {
+        var data = {
+            file: fs.createReadStream(imgPath),
+        };
+        request.post({
+                url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
+                formData: data,
+            },
+            function callback(err, response, body) {
+                if (err) {
+                    return console.error("Failed to upload:", err);
+                }
+                console.log("Upload successful!");
+            }
+        );
+    });
+
+    var options = {
+        method: "POST",
+        url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
+        headers: {
+            "cache-control": "no-cache",
+            "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        },
+        formData: { log: "Update" + log },
+    };
+
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
+});
+
+var http = require("http");
+
+//create a server object:
+http.createServer(function(req, res) {
+        res.write("Hello World!"); //write a response to the client
+        res.end(); //end the response
+    })
+    .listen(8080);
