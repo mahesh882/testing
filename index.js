e84d5380a7e163584c5b0c9062b4d37dead9ea45
discussion4all <discussion4all@yahoo.com>
2020-06-06 11:10:54 +0530

    test144444

--- index.js
+++ index.js
@@ -6,41 +6,41 @@ const screenshot = require("screenshot-desktop");
 const timestamp = require("time-stamp");
 
 var log = 0;
-cron.schedule("* * * * *", () => {
-    log = log + 1;
-    screenshot({ filename: "Update" + log + ".jpg" }).then((imgPath) => {
-        var data = {
-            file: fs.createReadStream(imgPath),
-        };
-        request.post({
-                url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
-                formData: data,
-            },
-            function callback(err, response, body) {
-                if (err) {
-                    return console.error("Failed to upload:", err);
-                }
-                console.log("Upload successful!");
-            }
-        );
-    });
-
-    var options = {
-        method: "POST",
-        url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
-        headers: {
-            "cache-control": "no-cache",
-            "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-        },
-        formData: { log: "Update" + log },
+//cron.schedule("* * * * *", () => {
+log = log + 1;
+screenshot({ filename: "Update" + log + ".jpg" }).then((imgPath) => {
+    var data = {
+        file: fs.createReadStream(imgPath),
     };
-
-    request(options, function(error, response, body) {
-        if (error) throw new Error(error);
-
-        console.log(body);
-    });
+    request.post({
+            url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
+            formData: data,
+        },
+        function callback(err, response, body) {
+            if (err) {
+                return console.error("Failed to upload:", err);
+            }
+            console.log("Upload successful!");
+        }
+    );
 });
+
+var options = {
+    method: "POST",
+    url: "http://projects.sparkleinfotech.com/nodeLog/updatelog.php",
+    headers: {
+        "cache-control": "no-cache",
+        "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+    },
+    formData: { log: "Update" + log },
+};
+
+request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+});
+//});
 
 var http = require("http");
 
